ef1febe2dfce96a181ef8a66266cb771e30a4f38
Chaoyue Huang <ihuangcy@gmail.com>
2020-02-13 17:53:21 -0500

    first push

---
+++ sketch.js
@@ -0,0 +1,132 @@
+var font;
+var vehicles = [];
+
+var texts = ['Chaoyue', 'loves', 'Yang', 'forever', '!!!'];
+var nextT = 0;
+var maxChangeForce = 20;
+
+var instructions = [];
+var insText = '';
+
+function preload() {
+  font = loadFont('KeepCalm-Medium.ttf');
+}
+
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+    background(0);
+
+    var bounds = font.textBounds(texts[nextT], 0, 0, 192);
+    var posx = width / 2 - bounds.w / 2;
+    var posy = height / 2 + bounds.h / 2;
+
+    var points = font.textToPoints(texts[nextT], posx, posy, 192, {
+        sampleFactor: 0.1
+    });
+
+    for (var i = 0; i < points.length; i++) {
+        var pt = points[i];
+        var vehicle = new Vehicle(pt.x, pt.y);
+        vehicles.push(vehicle);
+    }
+
+    var boundsIns = font.textBounds(insText, 0, 0, 30);
+    var posxIns = width / 2 - boundsIns.w / 2;
+    var posyIns = height / 6 + boundsIns.h / 2;
+
+    var insAr = split(insText, ' ');
+
+    for (var i = 0; i < insAr.length; i++) {
+        var bounds2 = font.textBounds(insAr[i], 0, 0, 30);
+        var posx2 = posxIns;
+        var posy2 = posyIns;
+
+        posxIns += bounds2.w + 10;
+
+        var points2 = font.textToPoints(insAr[i], posx2, posy2, 30, {
+            sampleFactor: 0.3
+        });
+
+        for (var j = 0; j < points2.length; j++) {
+            var pt = points2[j];
+            var v = new Vehicle(pt.x, pt.y, 3);
+            instructions.push(v);
+        }
+    }
+    var time;
+    time = createElement('h1', '2020.02.14 ❤️');
+    time.position(windowWidth/2-textWidth(time),170);
+}
+
+function draw() {
+    background(255);
+
+    for (var i = 0; i < instructions.length; i++) {
+        var v = instructions[i];
+        v.behaviors();
+        v.update();
+        v.show();
+    }
+
+    for (var i = 0; i < vehicles.length; i++) {
+        var v = vehicles[i];
+        v.behaviors();
+        v.update();
+        v.show();
+    }
+}
+
+function mouseClicked() {
+    nextT++;
+    if (nextT > texts.length - 1) {
+        nextT = 0;
+    }
+
+    var bounds = font.textBounds(texts[nextT], 0, 0, 192);
+    var posx = width / 2 - bounds.w / 2;
+    var posy = height / 2 + bounds.h / 2;
+
+    var points = font.textToPoints(texts[nextT], posx, posy, 192, {
+        sampleFactor: 0.1
+    });
+
+    if (points.length < vehicles.length) {
+        var toSplice = vehicles.length - points.length;
+        vehicles.splice(points.length - 1, toSplice);
+
+        for (var i = 0; i < points.length; i++) {
+            vehicles[i].target.x = points[i].x;
+            vehicles[i].target.y = points[i].y;
+
+            var force = p5.Vector.random2D();
+            force.mult(random(maxChangeForce));
+            vehicles[i].applyForce(force);
+        }
+    } else if (points.length > vehicles.length) {
+
+        for (var i = vehicles.length; i < points.length; i++) {
+            var v = vehicles[i - vehicles.length].clone();
+
+            vehicles.push(v);
+        }
+
+        for (var i = 0; i < points.length; i++) {
+            vehicles[i].target.x = points[i].x;
+            vehicles[i].target.y = points[i].y;
+
+            var force = p5.Vector.random2D();
+            force.mult(random(maxChangeForce));
+            vehicles[i].applyForce(force);
+        }
+
+    } else {
+        for (var i = 0; i < points.length; i++) {
+            vehicles[i].target.x = points[i].x;
+            vehicles[i].target.y = points[i].y;
+
+            var force = p5.Vector.random2D();
+            force.mult(random(maxChangeForce));
+            vehicles[i].applyForce(force);
+        }
+    }
+}
